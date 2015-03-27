15b2508b3ebdc8ec5a1d054c9e4188eb97880113
dmills <dmills@iastate.edu>
2015-03-26 20:31:24 -0500

    adding database support and server functions

--- channel.php
+++ channel.php
@@ -1,5 +1,23 @@
 <?php include 'parts/header.php'; ?>
+<?php
+$channel_name = $_GET['c']; // the current channel
+$con          = new mywrap_con(); // a mysql wrapper object
+$channel      = DJ::get_channel($con, $channel_name); // the channel object
 
-<h2> welcome to ... <?php echo $_GET['c']; ?> </h2>
+if (!$channel) {
+  $channel = DJ::create_channel($con, $channel_name); // create a new channel
+}
+?>
+<h2> Welcome to <?php echo htmlspecialchars($channel_name); ?> </h2>
 
+<?php if ($channel['owner']) : ?>
+  <p> You own this channel </p>
+  <?php if (!count($channel['tracks'])) : ?>
+    <p> You do not have any tracks in this channel </p>
+  <?php endif; ?>
+<?php else : ?>
+  <p> You don't own this channel </p>
+<?php endif; ?>
+
+<pre><?php print_r ($channel); ?></pre>
 <?php include 'parts/footer.php'; ?>
--- parts/header.php
+++ parts/header.php
@@ -1,3 +1,6 @@
+<?php session_start(); ?>
+<?php require_once './lib/mysqli-wrapper/mywrap.php'; ?>
+<?php require_once './lib/dj.php'; ?>
 <!doctype html>
 <html>
   <head>
