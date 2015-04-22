25e9198e4ed7f14a78fda97bb34558e571c70732
GregBreuss <gjbreuss@iastate.edu>
2015-04-22 17:00:13 -0500

    Updated documentation for footer.php

--- parts/footer.php
+++ parts/footer.php
@@ -1,3 +1,4 @@
+<!-- End of the website page. Included on all pages of the website -->
     </div>
   </div>
   <div class='page-footer'>
@@ -9,14 +10,14 @@
     </div>
   </div>
 
-  <!-- fonts -->
+  <!-- Link to fonts -->
   <link href='http://fonts.googleapis.com/css?family=Roboto:400,500,700,400italic|Fjord+One' rel='stylesheet' type='text/css'>
 
   <!-- vendor scripts -->
   <script src='https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
   <script src='<?php echo $root; ?>src/js/underscore.min.js'></script>
 
-  <!-- local scripts -->
+  <!-- local scripts used for website functionality-->
   <script src='<?php echo $root; ?>src/js/dj.js'></script>
   <script src='<?php echo $root; ?>src/js/dj-api.js'></script>
   <script src='<?php echo $root; ?>src/js/dj-controls.js'></script>
@@ -24,7 +25,7 @@
   <script src='<?php echo $root; ?>src/js/dj-view.js'></script>
   <script src='<?php echo $root; ?>src/js/dj-tracks.js'></script>
 
-  <!-- players -->
+  <!-- players scripts used for the player functionality -->
   <script src='<?php echo $root; ?>src/js/dj-player-youtube.js'></script>
   <script src='<?php echo $root; ?>src/js/dj-player-audio.js'></script>
 
