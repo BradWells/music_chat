7f00803b8c372ce4e50d2f4c26550bb0ef3f08af
GregBreuss <gjbreuss@iastate.edu>
2015-04-22 16:56:29 -0500

    Added documentation to listener.php

--- parts/channel/listener.php
+++ parts/channel/listener.php
@@ -1,10 +1,11 @@
+<!-- This page is displayed when a user requests a channel that has already been made. If they are not the owner of the page, they are a "listener" -->
 <h4>
   <span class='channel-title-modifier'>loading: </span>
-  <span class='channel-title'><?php echo htmlspecialchars($channel_name); ?></span>
+  <span class='channel-title'><?php echo htmlspecialchars($channel_name); ?></span> <!-- Displays channel name -->
 </h4>
 <div class='channel-container hide'>
-  <button type='button' id='channel-container-sync'>Start Listening </button>
+  <button type='button' id='channel-container-sync'>Start Listening </button> <!-- Button to initiate the user to start listening to the DJ's music. -->
   <p class='channel-notification'></p>
-  <?php include $root . 'parts/channel/player.php'; ?>
+  <?php include $root . 'parts/channel/player.php'; ?> <!-- Displays the audio/video player -->
 </div>
-<?php include $root . 'parts/channel/loader.php'; ?>
+<?php include $root . 'parts/channel/loader.php'; ?> <!-- Displays while the viewer page is loading -->
