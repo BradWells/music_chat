fa036bbe3c2efec81714b5a7d08c2d8775e43a9d
GregBreuss <gjbreuss@iastate.edu>
2015-04-22 17:01:50 -0500

    Added documentation to loader.php

--- parts/channel/loader.php
+++ parts/channel/loader.php
@@ -1,3 +1,4 @@
+<!-- Used as an intermediate display while requested page is being built and loaded -->
 <div class='loader channel-loader-container'>
   <i class='fa fa-2x fa-cog fa-spin'></i>
 </div>
